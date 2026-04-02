8b03d96afcb5e4cad0e253edcc964cfd85997f24
Rajat <rajat804@gmail.com>
2026-04-02 12:42:26 +0530

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,18 +8,17 @@ import { DashboardShell } from "@/components/DashboardShell";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Cargomax – Shipping & Logistics Admin Dashboard Next.js Template",
-  description:
-    "Cargomax is a modern and responsive Shipping & Logistics Admin Dashboard Template designed for cargo management, freight tracking, warehouse control, and logistics operations. Built with clean UI components and advanced features, Cargomax offers real-time shipment monitoring, driver assignments, order tracking, fleet status, and warehouse insights. Ideal for logistics companies, transportation services, and supply chain dashboards, this template ensures a seamless and intuitive admin experience. Fully responsive, customizable, and developer-friendly – boost your logistics platform with Cargomax today.",
+  title: "Cargomax – Shipping & Logistics Admin Dashboard",
+  description: "Logistics admin dashboard",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <DashboardShell>{children}</DashboardShell>
